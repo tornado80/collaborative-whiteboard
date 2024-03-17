ca3fdd9eda7d26226e3d60cc6d9d6711381e2b6a
Sonika Baniya <sonika.baniya.sb@gmail.com>
2024-03-17 19:40:22 +0200

    handle_event updated

--- backend/apps/backend/src/websocket_event_handlers.erl
+++ backend/apps/backend/src/websocket_event_handlers.erl
@@ -7,19 +7,24 @@
 ]).
 
 handle_event(
-        #event{eventType = 'begin', eventId = _EventId, eventContent = Event}, 
+        #event{eventType = 'begin', eventId = EventId, eventContent = Event}, 
         #websocket_handler_state{boardManagerPid = Pid} = State) ->
     case {Event#begin_event.sessionType, Event#begin_event.sessionToken, Event#begin_event.lastEventId} of
         {new, _, _} -> 
-            {ok, SessionToken} = board_controller_service:new_session(Pid),
+            {ok, SessionToken, UserId} = board_controller_service:new_session(Pid),
 
-            {[{text, welcomeUser}], State#websocket_handler_state{sessionToken = SessionToken}};
+            WelcomeData = jsone:encode({[{<<"eventId">>, EventId+1}, {<<"eventType">>, <<"welcomeUser">>},{<<"sessionToken">>, SessionToken}, {<<"userId">>, UserId}]}),
+            
+            {[{ text, WelcomeData}], State#websocket_handler_state{sessionToken = SessionToken}};
+            
         {continue, undefined, _} -> 
             {[{close, <<"session token is not provided">>}], State};
         {continue, _, undefined} -> 
             {[{close, <<"last event id is not provided">>}], State};
         {continue, SessionToken0, LastEventId} when is_integer(LastEventId), is_binary(SessionToken0) -> 
-            {ok, SessionToken1} = board_controller_service:continue_session(Pid, SessionToken0, LastEventId),
+            {ok, SessionToken1, UserId} = board_controller_service:continue_session(Pid, SessionToken0, LastEventId),
+             jsone:encode({[{<<"eventId">>, EventId+1}, {<<"eventType">>, <<"welcomeUser">>},{<<"sessionToken">>, SessionToken1}, {<<"userId">>, UserId}]}),
+
             {[{text, welcomeUser}], State#websocket_handler_state{sessionToken = SessionToken1}};
         {continue, _, _} ->
             {[{close, <<"malformed begin event">>}], State};
