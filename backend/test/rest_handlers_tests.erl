731a75ccb17af84700b30c641e52b84de3eee306
Sonika Baniya <sonika.baniya.sb@gmail.com>
2024-03-22 18:19:30 +0200

    test case

--- backend/test/rest_handlers_tests.erl
+++ backend/test/rest_handlers_tests.erl
@@ -37,3 +37,21 @@ create_board_handler_test() ->
     gun:close(Pid),
     application:stop(backend),
     application:stop(gun).
+
+websocket_handler_test() ->
+    % Arrange
+    application:ensure_all_started(backend),
+    application:ensure_all_started(gun),
+
+    % Act
+    {ok, Pid} = gun:open("localhost", 8080),
+    StreamRef = gun:ws_upgrade(Pid, "/api/ws/boards/"),
+    {ok, Message} = gun:await(Pid, StreamRef),
+
+    % Assert
+    ?assertMatch({ws_upgrade, {response, nofin, 101, _Headers}}, Message),
+
+    % Cleanup
+    gun:close(Pid),
+    application:stop(backend),
+    application:stop(gun).
