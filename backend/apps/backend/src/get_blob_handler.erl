160d4b460cbbe7f071d4fd991bd3ed0db0d43a87
Amirhosein Rajabi <amirhosein.rajabi@aalto.fi>
2024-03-28 20:39:56 +0200

    Fix infinite loop in get board handler

--- backend/apps/backend/src/get_blob_handler.erl
+++ backend/apps/backend/src/get_blob_handler.erl
@@ -36,7 +36,6 @@ is_request_malformed('is board id valid uuid?', Req, State = #blob_handler_state
         false -> {true, Req, State};
         true -> is_request_malformed('is blob id provided?', Req, State)
     end;
-
 is_request_malformed('is blob id provided?', Req, State) ->
     BlobId = cowboy_req:binding(blobId, Req),
     case blobId of 
@@ -47,7 +46,7 @@ is_request_malformed('is blob id provided?', Req, State) ->
 is_request_malformed('is blob id valid uuid?', Req, State = #blob_handler_state{blobId = BlobId}) ->
     case utility:is_valid_uuid(BlobId) of
         false -> {true, Req, State};
-        true -> is_request_malformed('is blob id provided?', Req, State)
+        true -> {false, Req, State}
     end.
 
 
@@ -58,17 +57,22 @@ is_request_malformed('is blob id valid uuid?', Req, State = #blob_handler_state{
 %     end.
 
 resource_exists(Req, State = #blob_handler_state{boardId = BoardId, blobId = BlobId}) ->
+    lager:info("Checking if blob ~p exists at board ~p cache service", [BlobId, BoardId]),
     case boards_manager_service:try_get_board_cache_service(BoardId) of
         notfound -> {false, Req, State};
         service_not_available ->
+            lager:info("Board ~p cache service is not available now. Retry in a few seconds.", [BoardId]),
             Req1 = cowboy_req:reply(503, #{<<"retry-after">> => 10},
                 <<"Board cache service is not available now. Retry in a few seconds.">>, Req),
             {stop, Req1, State};
         {ok, Pid} ->
+            lager:info("Trying get blob ~p from board ~p cache service", [BlobId, BoardId]),
             case board_cache_service:try_get_blob(Pid, BlobId) of
                 notfound ->
+                    lager:info("Blob ~p not found at board ~p cache service", [BlobId, BoardId]),
                     {false, Req, State};
                 {ok, Blob} ->
+                    lager:info("Blob ~p received from board ~p cache service", [BlobId, BoardId]),
                     {true, Req, State#blob_handler_state{blob = Blob}}
             end
     end.
